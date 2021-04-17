c9d0beebe65debd0512dbe4515f688228b9a942f
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-04-17 17:00:31 -0300

    C02P01-1

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { StormGlass } from '@src/clients/StormGlass';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture
+  from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StromGlass Client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.741258;
+    const lng = 151.562158;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  })
+})
--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -5,5 +5,5 @@ const files = path.resolve(__dirname, '../../');
 
 modulesAlias.addAliases({
   '@src': path.join(files, 'src'),
-  '@test': path.join(files, 'test')
+  '@test': path.join(files, 'test'),
 });
