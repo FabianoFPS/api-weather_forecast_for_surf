d991b4d166641ab558662e0ac5f9bb4bba2e6381
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-05-05 18:47:31 -0300

    C04P01

--- src/services/Forecast.ts
+++ src/services/Forecast.ts
@@ -17,26 +17,50 @@ export interface Beach {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
-  public async processForecastBeaches(beaches: Beach[]): Promise<BeachForecast[]> {
+  public async processForecastBeaches(
+    beaches: Beach[]
+  ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
-    for(const beach of beaches) {
+    for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-      const enrichedBeachData = points.map(e => ({
+      const enrichedBeachData = points.map((e) => ({
         ...{
           lat: beach.lat,
           lng: beach.lng,
           name: beach.name,
           position: beach.position,
-          rating: 1
+          rating: 1,
         },
-        ...e
+        ...e,
       }));
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
 
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+
+  return forecastByTime;
   }
 }
