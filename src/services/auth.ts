b50fd567d76027e29c8507efc1fbd84a05f83023
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-06-28 15:22:05 -0300

    [C05P04]

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export default class AuthService {
   public static async hashPassword(
@@ -14,5 +16,10 @@ export default class AuthService {
   ): Promise<boolean> {
     return await bcrypt.compare(password, hashedPassword);
   }
-}
 
+  public static generateToken(payload: Record<string, unknown>): string {
+    const secret: string = config.get('App.auth.key');
+    const expiresIn: number = config.get('App.auth.tokenExpiresIn');
+    return jwt.sign(payload, secret, { expiresIn });
+  }
+}
