235990be6b4e351321532b7b84e42bc3bec5c624
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-09-04 12:14:29 -0300

    C07P02

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,71 @@
 import { SetupServer } from './server';
 import config from 'config';
+import logger from './logger';
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+function exitApp(
+  status: ExitStatus,
+  error?: unknown,
+  isUncaughtException = false
+) {
+  const log = {
+    ['1']: () => logger.error(`App exited with error: ${error}`),
+    ['0']: () => logger.info(`App exited with success`),
+  };
+
+  if (isUncaughtException) {
+    logger.error(`App exiting due to an uncaught exception: ${error}`);
+  } else {
+    log[status]();
+  }
+
+  process.exit(status);
+}
+
+function exitAppWithSuccess() {
+  exitApp(ExitStatus.Success);
+}
+
+function exitAppWithFailure(error: unknown) {
+  exitApp(ExitStatus.Failure, error);
+}
+
+function exitAppUncaughtException(error: unknown) {
+  exitApp(ExitStatus.Failure, error, true);
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due to an unhandled promise: ${promise} and reason ${reason}`
+  );
+  throw reason;
+});
+
+process.on('uncaughtException', exitAppUncaughtException);
 
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer(config.get('App.port'));
+    await server.init();
+    server.start();
+
+    const exitSignals: Array<NodeJS.Signals> = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+
+    exitSignals.map((sig) => {
+      process.on(sig, async () => {
+        try {
+          await server.close();
+          exitAppWithSuccess();
+        } catch (error) {
+          exitAppWithFailure(error);
+        }
+      });
+    });
+  } catch (error) {
+    exitAppWithFailure(error);
+  }
 })();
